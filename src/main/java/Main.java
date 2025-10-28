2765fef499e5733e0a47182f6d6c74c0797139a0
Palak Kumar <Palak.kumar@somaiya.edu>
2025-10-28 20:03:08 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,4 @@
-package main.java;// src/main/java/Main.java
+package main.java;
 import main.java.view.AppFrame;
 
 import javax.swing.*;
@@ -6,14 +6,14 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 
-// src/main/java/Main.java
+
 
 import javax.swing.*;
 
 
 public class Main {
     public static void main(String[] args) {
-        // Ensure data/ and reports/ directory exist
+        
         try {
             Files.createDirectories(new File("data").toPath());
             Files.createDirectories(new File("reports").toPath());
